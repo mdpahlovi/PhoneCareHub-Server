696e21815bd3b77094a6df45e78a07fa715ba302
MD Pahlovi <mdpahlovi07@gmail.com>
2023-10-27 01:42:37 +0600

    [Delete] Review Validation

--- src/app/modules/review/review.route.ts
+++ src/app/modules/review/review.route.ts
@@ -1,22 +1,15 @@
 import express from "express";
 import { USER_ROLE } from "../../../enums/user";
 import auth from "../../middlewares/auth";
-import validateRequest from "../../middlewares/validateRequest";
 import { ReviewController } from "./review.controller";
-import { ReviewValidation } from "./review.validation";
 
 const router = express.Router();
 
-router.post(
-    "/create",
-    auth(USER_ROLE.USER, USER_ROLE.ADMIN),
-    validateRequest(ReviewValidation.createReview),
-    ReviewController.createReview,
-);
+router.post("/create", auth(USER_ROLE.USER, USER_ROLE.ADMIN), ReviewController.createReview);
 
 router.get("/", ReviewController.getAllReview);
 
-router.patch("/:id", auth(USER_ROLE.USER, USER_ROLE.ADMIN), validateRequest(ReviewValidation.updateReview), ReviewController.updateReview);
+router.patch("/:id", auth(USER_ROLE.USER, USER_ROLE.ADMIN), ReviewController.updateReview);
 
 router.delete("/:id", auth(USER_ROLE.USER, USER_ROLE.ADMIN), ReviewController.deleteReview);
 
